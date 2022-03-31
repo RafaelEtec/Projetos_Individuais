9238ccacc7a334313d7f7a8c7b41d06bf7c7f04e
raf <raf@DESKTOP-PN9HH5P>
2022-03-31 11:24:23 -0300

    menu atualizado

--- src/main/java/Funções/menu.java
+++ src/main/java/Funções/menu.java
@@ -8,14 +8,32 @@ public class menu {
         Scanner ent = new Scanner(System.in);
         
         System.out.println(
-                  "O que você deseja avaliar?: \n"
-                + "<a> Sorting System\n");
+                  " |------ O que você deseja avaliar? ------| \n"
+                + "<A> Sorting System (Crescente)      \n"
+                + "<B> Sorting System (Decrescente)    \n"
+                + "<> \n"
+                + "<> \n"
+                + "<> \n"
+                + "<S> Sair \n");
         String escolha = ent.next();
         
         switch (escolha.toLowerCase()) {
             case "a":
-                System.out.println(" ---| Sorting System |--- \n");
-                Funções.Todas.sortingSystem();
+                System.out.println(" ---| Sorting System (Crescente) |--- \n");
+                /**
+                 * Ordena os números inseridos de forma Crescente
+                **/
+                Funções.Todas.sortingSystem(Funções.Entrada.entIntArray(), escolha);
+                break;
+            case "b":
+                System.out.println(" ---| Sorting System (Decrescente) |--- \n");
+                /**
+                 * Ordena os números inseridos de forma Decrescente
+                **/
+                Funções.Todas.sortingSystem(Funções.Entrada.entIntArray(), escolha);
+                break;
+            case "s":
+                System.out.println(" ---| Saindo do Programa |--- \n");
                 break;
             default:
                 System.out.println("Ocorreu um erro!");
