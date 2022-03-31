6294f11b08469f858f7a26d4e2ad2a30fb35de56
raf <raf@DESKTOP-PN9HH5P>
2022-03-31 11:24:46 -0300

    função Sorting System finalizada

--- src/main/java/Funções/Todas.java
+++ src/main/java/Funções/Todas.java
@@ -1,33 +1,28 @@
 package Funções;
-import java.util.Scanner;
 /** 
    por Rafael Ferreira Goulart
 **/
 public class Todas {
-    public static void sortingSystem() {
-        Scanner ent = new Scanner(System.in);
-        
-        System.out.println("Insira a quantidade de números a serem ordenados: ");
-        int tamanhoArray = ent.nextInt();
-        int[] sequencia = new int[tamanhoArray];
-        
-        for (int pos = 0; pos < tamanhoArray; pos++) {
-            System.out.println("pos: "+pos+" - Insira o número da posição: ");
-            int num = ent.nextInt();
-            sequencia[pos] = num;
-        }
-        
-        for (int pos = 0; pos < tamanhoArray; pos++) {
-            for (int proxPos = pos; proxPos < tamanhoArray; proxPos++) {
-                int guarda;
-                if (sequencia[proxPos] < sequencia[pos]) {
-                    guarda = sequencia[pos];
-                    sequencia[pos] = sequencia[proxPos];
-                    sequencia[proxPos] = guarda;
+    public static void sortingSystem(int[] entIntArray, String escolha) {
+        int guarda;
+        for (int pos = 0; pos < entIntArray.length ; pos++) {
+            for (int proxPos = pos; proxPos < entIntArray.length; proxPos++) {
+                if (escolha.toLowerCase().equals("a")) {
+                    if (entIntArray[proxPos] < entIntArray[pos]) {
+                        guarda = entIntArray[pos];
+                        entIntArray[pos] = entIntArray[proxPos];
+                        entIntArray[proxPos] = guarda;
+                    }
+                } else if (escolha.toLowerCase().equals("b")) {
+                    if (entIntArray[proxPos] > entIntArray[pos]) {
+                        guarda = entIntArray[pos];
+                        entIntArray[pos] = entIntArray[proxPos];
+                        entIntArray[proxPos] = guarda;
+                    }
                 }
             }
             
-            System.out.println("pos: "+pos+" - "+sequencia[pos]);
+            System.out.println("pos: "+pos+" - "+entIntArray[pos]);
         }
     }
 }
