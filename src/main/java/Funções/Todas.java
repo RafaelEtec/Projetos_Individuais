28cb8ca20c6dc9f4c087858f4ceafa0e0bcd4da3
raf <raf@DESKTOP-PN9HH5P>
2022-04-01 11:31:20 -0300

    função Sorting Text criada

--- src/main/java/Funções/Todas.java
+++ src/main/java/Funções/Todas.java
@@ -25,4 +25,27 @@ public class Todas {
             System.out.println("pos: "+pos+" - "+entIntArray[pos]);
         }
     }
+    
+    public static void sortingText(String[] entStringArray, String escolha) {
+        String guarda;
+        for (int pos = 0; pos < entStringArray.length; pos++) {
+            for (int proxPos = pos; proxPos < entStringArray.length; proxPos++) {
+                if (escolha.toLowerCase().equals("c")) {
+                    if (entStringArray[proxPos].length() < entStringArray[pos].length()) {
+                        guarda = entStringArray[pos];
+                        entStringArray[pos] = entStringArray[proxPos];
+                        entStringArray[proxPos] = guarda;
+                    }
+                } else if (escolha.toLowerCase().equals("d")) {
+                    if (entStringArray[proxPos].length() > entStringArray[pos].length()) {
+                        guarda = entStringArray[pos];
+                        entStringArray[pos] = entStringArray[proxPos];
+                        entStringArray[proxPos] = guarda;
+                    }
+                }
+            }
+            
+            System.out.println("pos: "+pos+" - "+entStringArray[pos]+" - "+entStringArray[pos].length()+" letras");
+        }
+    }
 }
--- src/main/java/OrdenacaoJava/Teste.java
+++ src/main/java/OrdenacaoJava/Teste.java
@@ -5,6 +5,10 @@ import java.util.Scanner;
 **/
 public class Teste {
     public static void main(String[] args) {
+        textoMaior();
+    }
+    
+    public static void sortingSystemTest() {
         Scanner ent = new Scanner(System.in);
                 
         System.out.println("Insira a quantidade de números a serem ordenados: ");
@@ -28,6 +32,33 @@ public class Teste {
             }
             
             System.out.println("pos: "+pos+" - "+sequencia[pos]);
-        }   
+        } 
+    }
+    
+    public static void textoMaior() {
+        Scanner ent = new Scanner(System.in);
+        
+        System.out.println("Insira a quantidade de texto a ser armazenado: ");
+        int tamanhoArray = ent.nextInt();
+        String[] entStringArray = new String[tamanhoArray];
+        
+        for (int pos = 0; pos < tamanhoArray; pos++) {
+            System.out.println("pos: "+pos+" - Insira o texto da posição: ");
+            String text = ent.next();
+            entStringArray[pos] = text;
+        }
+        
+        for (int pos = 0; pos < tamanhoArray; pos++) {
+            for (int proxPos = pos; proxPos < tamanhoArray; proxPos++) {
+                String guarda;
+                if (entStringArray[proxPos].length() < entStringArray[pos].length()) {
+                    guarda = entStringArray[pos];
+                    entStringArray[pos] = entStringArray[proxPos];
+                    entStringArray[proxPos] = guarda;
+                }
+            }
+            
+            System.out.println("pos: "+pos+" - "+entStringArray[pos]+" - "+entStringArray[pos].length()+" letras");
+        }
     }
 }
