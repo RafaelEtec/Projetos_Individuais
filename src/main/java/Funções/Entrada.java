e39f922c83a6630bd80e1dc2a5312320f3cf5f48
raf <raf@DESKTOP-PN9HH5P>
2022-04-08 13:55:31 -0300

    entInt e entDouble criados 	para entrada de 2 valores

--- src/main/java/Funções/Entrada.java
+++ src/main/java/Funções/Entrada.java
@@ -35,4 +35,30 @@ public class Entrada {
         
         return entStringArray;
     }
+    
+    public static int[] entInt2val() {
+        Scanner ent = new Scanner(System.in);
+        
+        int[] entInt2val = new int[2];
+        
+        System.out.println("Insira o primeiro valor: ");
+        entInt2val[0] = ent.nextInt();
+        System.out.println("Insira o segundo valor: ");
+        entInt2val[1] = ent.nextInt();
+        
+        return entInt2val;
+    }
+    
+    public static double[] entDouble2val() {
+        Scanner ent = new Scanner(System.in);
+        
+        double[] entDouble2val = new double[2];
+        
+        System.out.println("Insira o primeiro valor: ");
+        entDouble2val[0] = ent.nextDouble();
+        System.out.println("Insira o segundo valor: ");
+        entDouble2val[1] = ent.nextDouble();
+        
+        return entDouble2val;
+    }
 }
